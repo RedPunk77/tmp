1df4502ab326e34d5d9b350b1962bd49c1eeffbe
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2025-02-24 17:05:14 +0300

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int sum(int a, int b) {
+  return a+b;
+}
+int mult(int x, int y) {
+  return x*y;
+}
+int sub(int k, int m) {
+  if (k >= m) return k-m;
+  else return m-k;
+}
